6d74cfd8d09b35c50f8bbce2a1d049ed4d2978c3
Alex <dharmaturtle@gmail.com>
2020-12-24 11:16:14 -0600

    Cleaned up tests and deleted AssertExceptionRegexAttribute

--- src/Hedgehog.Xunit/AssertExceptionRegexAttribute.fs
+++
@@ -1,7 +0,0 @@
-namespace Hedgehog.Xunit
-
-open System
-
-[<AttributeUsage(AttributeTargets.Method ||| AttributeTargets.Property, AllowMultiple = false)>]
-type internal AssertExceptionRegexAttribute(regexPattern: string) =
-  inherit Attribute()
--- tests/Hedgehog.Xunit.Tests/PropertyTests.fs
+++ tests/Hedgehog.Xunit.Tests/PropertyTests.fs
@@ -1,81 +1,89 @@
 namespace Hedgehog.Xunit.Tests
 
+open Xunit
+open System
+open Hedgehog
 open Hedgehog.Xunit
 
-module PropertyModuleTests =
+module Common =
+  let [<Literal>] skipReason = "Skipping because it's just here to be the target of a [<Fact>] test"
+  let assertMatch (e: Exception) pattern =
+    System.Text.RegularExpressions.Regex.IsMatch(e.Message, pattern)
+    |> Assert.True
 
-  [<Property>]
-  [<AssertExceptionRegex("""\*\*\* Failed! Falsifiable \(after 1 test\):\s+\(0\)""")>]
-  let ``fails for false`` (_: int) =
-    false
+open Common
+
+module ``Property module tests`` =
+
+  type private Marker = class end
+  let getMethod = typeof<Marker>.DeclaringType.GetMethod
+  let assertMatch methodName =
+    Assert.Throws<FailedException>(fun () -> PropertyHelper.check (getMethod methodName) null)
+    |> assertMatch
+
+  [<Property(Skip = skipReason)>]
+  let ``fails for false, skipped`` (_: int) = false
+  [<Fact>]
+  let ``fails for false`` () =
+    assertMatch
+      (nameof(``fails for false, skipped``))
+      """\*\*\* Failed! Falsifiable \(after 1 test\):\s+\(0\)"""
 
   [<Property>]
   let ``Can generate an int`` (i: int) =
     printfn "Test input: %i" i
 
-  [<Property>]
-  [<AssertExceptionRegex("""\*\*\* Failed! Falsifiable \(after \d+ tests(?: and \d+ shrinks?)?\):\s+\(50\)""")>]
-  let ``Can shrink an int`` (i: int) =
+  [<Property(Skip = skipReason)>]
+  let ``Can shrink an int, skipped`` (i: int) =
     if i >= 50 then failwith "Some error."
+  [<Fact>]
+  let ``Can shrink an int`` () =
+    assertMatch
+      (nameof(``Can shrink an int, skipped``))
+      """\*\*\* Failed! Falsifiable \(after \d+ tests(?: and \d+ shrinks?)?\):\s+\(50\)"""
 
   [<Property>]
   let ``Can generate two ints`` (i1: int, i2: int) =
     printfn "Test input: %i, %i" i1 i2
 
-  [<Property>]
-  [<AssertExceptionRegex("""\*\*\* Failed! Falsifiable \(after \d+ tests(?: and \d+ shrinks?)?\):\s+\(10, 20\)""")>]
-  let ``Can shrink both ints`` (i1: int, i2: int) =
+  [<Property(Skip = skipReason)>]
+  let ``Can shrink both ints, skipped`` (i1: int, i2: int) =
     if i1 >= 10 &&
        i2 >= 20 then failwith "Some error."
+  [<Fact>]
+  let ``Can shrink both ints`` () =
+    assertMatch
+      (nameof(``Can shrink both ints, skipped``))
+      """\*\*\* Failed! Falsifiable \(after \d+ tests(?: and \d+ shrinks?)?\):\s+\(10, 20\)"""
   
   [<Property>]
   let ``Can generate an int and string`` (i: int, s: string) =
     printfn "Test input: %i, %s" i s
   
-  [<Property>]
-  [<AssertExceptionRegex("""\*\*\* Failed! Falsifiable \(after \d+ tests(?: and \d+ shrinks?)?\):\s+\(2, \"b\"\)""")>]
-  let ``Can shrink an int and string`` (i: int, s: string) =
+  [<Property(Skip = skipReason)>]
+  let ``Can shrink an int and string, skipped`` (i: int, s: string) =
     if i >= 2 && s.Contains "b" then failwith "Some error."
+  [<Fact>]
+  let ``Can shrink an int and string`` () =
+    assertMatch
+      (nameof(``Can shrink an int and string, skipped``))
+      """\*\*\* Failed! Falsifiable \(after \d+ tests(?: and \d+ shrinks?)?\):\s+\(2, \"b\"\)"""
 
-type PropertyClassTests(output: Xunit.Abstractions.ITestOutputHelper) =
+type ``Property class tests``(output: Xunit.Abstractions.ITestOutputHelper) =
 
-  [<Property>]
-  [<AssertExceptionRegex("""\*\*\* Failed! Falsifiable \(after 1 test\):\s+\(0\)""")>]
-  let ``fails for false`` (_: int) =
-    false
-  
   [<Property>]
   let ``Can generate an int`` (i: int) =
     sprintf "Test input: %i" i |> output.WriteLine
-  
-  [<Property>]
-  [<AssertExceptionRegex("""\*\*\* Failed! Falsifiable \(after \d+ tests(?: and \d+ shrinks?)?\):\s+\(50\)""")>]
-  let ``Can shrink an int`` (i: int) =
-    if i >= 50 then failwith "Some error."
   
   [<Property>]
   let ``Can generate two ints`` (i1: int, i2: int) =
     sprintf "Test input: %i, %i" i1 i2 |> output.WriteLine
   
   [<Property>]
-  [<AssertExceptionRegex("""\*\*\* Failed! Falsifiable \(after \d+ tests(?: and \d+ shrinks?)?\):\s+\(10, 20\)""")>]
-  let ``Can shrink both ints`` (i1: int, i2: int) =
-    if i1 >= 10 &&
-       i2 >= 20 then failwith "Some error."
-    
-  [<Property>]
   let ``Can generate an int and string`` (i: int, s: string) =
     sprintf "Test input: %i, %s" i s |> output.WriteLine
     
-  [<Property>]
-  [<AssertExceptionRegex("""\*\*\* Failed! Falsifiable \(after \d+ tests(?: and \d+ shrinks?)?\):\s+\(2, \"b\"\)""")>]
-  let ``Can shrink an int and string`` (i: int, s: string) =
-    if i >= 2 && s.Contains "b" then failwith "Some error."
-
-module ``Property with AutoGenConfig tests`` =
-  open Xunit
-  open Hedgehog
-  open System
+module ``Property module with AutoGenConfig tests`` =
 
   module NormalTests =
 
@@ -88,7 +96,6 @@ module ``Property with AutoGenConfig tests`` =
   module FailingTests =
 
     type private Marker = class end
-    let [<Literal>] skipReason = "Skipping because it's just here to be the target of a [<Fact>] test"
 
     type NonstaticProperty = member _.__ = GenX.defaults
     [<Property(typeof<NonstaticProperty>, skipReason)>]
@@ -97,7 +104,7 @@ module ``Property with AutoGenConfig tests`` =
     let ``Instance property fails`` () =
       let testMethod = typeof<Marker>.DeclaringType.GetMethod(nameof(``Instance property fails, skipped``))
       let e = Assert.Throws<Exception>(fun () -> PropertyHelper.check testMethod null)
-      Assert.Equal(e.Message, "Hedgehog.Xunit.Tests.Property with AutoGenConfig tests+FailingTests+NonstaticProperty must have exactly one static property that returns an AutoGenConfig")
+      Assert.Equal(e.Message, "Hedgehog.Xunit.Tests.Property module with AutoGenConfig tests+FailingTests+NonstaticProperty must have exactly one static property that returns an AutoGenConfig")
 
     type NonAutoGenConfig = static member __ = ()
     [<Property(typeof<NonAutoGenConfig>, skipReason)>]
@@ -106,4 +113,4 @@ module ``Property with AutoGenConfig tests`` =
     let ``Non AutoGenConfig static property fails`` () =
       let testMethod = typeof<Marker>.DeclaringType.GetMethod(nameof(``Non AutoGenConfig static property fails, skipped``))
       let e = Assert.Throws<Exception>(fun () -> PropertyHelper.check testMethod null)
-      Assert.Equal(e.Message, "Hedgehog.Xunit.Tests.Property with AutoGenConfig tests+FailingTests+NonAutoGenConfig must have exactly one static property that returns an AutoGenConfig")
+      Assert.Equal(e.Message, "Hedgehog.Xunit.Tests.Property module with AutoGenConfig tests+FailingTests+NonAutoGenConfig must have exactly one static property that returns an AutoGenConfig")
