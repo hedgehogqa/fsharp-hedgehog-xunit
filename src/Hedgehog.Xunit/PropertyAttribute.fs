f61489e05adb139e6bdf84da1344ef2fb662995e
Alex <dharmaturtle@gmail.com>
2021-01-10 11:20:59 -0600

    PropertiesAttribute inherits from just Attribute

--- src/Hedgehog.Xunit/PropertyAttribute.fs
+++ src/Hedgehog.Xunit/PropertyAttribute.fs
@@ -41,15 +41,38 @@ type PropertyAttribute(autoGenConfig, tests, skip) =
   member internal _.GetTests                    = _tests
 
 
-///Set a default AutoGenConfig or <tests> for all [<Property>] attributed methods in this class/module
+/// Set a default AutoGenConfig or <tests> for all [<Property>] attributed methods in this class/module
 [<AttributeUsage(AttributeTargets.Class, AllowMultiple = false)>]
 type PropertiesAttribute(autoGenConfig, tests) =
-  inherit PropertyAttribute(autoGenConfig, tests, null)
+  inherit Attribute() // don't inherit Property - it exposes members like Skip which are unsupported
+
+  let mutable _autoGenConfig: Type       option = autoGenConfig
+  let mutable _tests        : int<tests> option = tests
+
   new()                          = PropertiesAttribute(None              , None)
   new(autoGenConfig)             = PropertiesAttribute(Some autoGenConfig, None      )
   new(tests)                     = PropertiesAttribute(None              , Some tests)
   new(autoGenConfig:Type, tests) = PropertiesAttribute(Some autoGenConfig, Some tests)
   new(tests, autoGenConfig:Type) = PropertiesAttribute(Some autoGenConfig, Some tests)
+
+  // https://github.com/dotnet/fsharp/issues/4154 sigh
+  /// This requires a type with a single static member (with any name) that returns an AutoGenConfig.
+  ///
+  /// Example usage:
+  ///
+  /// ```
+  ///
+  /// type Int13 = static member AnyName = GenX.defaults |> AutoGenConfig.addGenerator (Gen.constant 13)
+  ///
+  /// [<Property(typeof<Int13>)>]
+  ///
+  /// let myTest (i:int) = ...
+  ///
+  /// ```
+  member          _.AutoGenConfig    with set v = _autoGenConfig <- Some v
+  member          _.Tests            with set v = _tests         <- Some v
+  member internal _.GetAutoGenConfig            = _autoGenConfig
+  member internal _.GetTests                    = _tests
 
 module internal PropertyHelper =
 
